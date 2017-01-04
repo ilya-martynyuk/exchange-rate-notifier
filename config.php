4bfaadcb1a4562aa66288c0797c0f5093b24293b
Ilya Martynyuk <ilyam@appstone.co.il>
2017-01-04 17:25:31 +0200

    Added common part of code

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'emailTo' => 'XXX',
+    'emailSubject' => 'Exchange rate has updated',
+    'smptHost' => 'smtp.gmail.com',
+    'smptPort' => 465,
+    'smptUsername' => 'XXX',
+    'smptPassword' => 'XXX',
+    'cacheFilePath' => __DIR__ . '/.cache'
+];
